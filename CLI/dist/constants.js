dae498ff590cd399cd08cccddf72b3747aa48350
sushant-wayal <sushantssr12345@gmail.com>
2025-03-25 14:16:30 +0530

    refactor: update references from Velvet UI to Velvix UI in CLI and related files

--- CLI/dist/constants.js
+++ CLI/dist/constants.js
@@ -1,4 +1,4 @@
-export const NAME = 'Velvet UI';
-export const BASE_URL = 'https://raw.githubusercontent.com/sushant-wayal/Velvet-UI/main/Components/';
+export const NAME = 'Velvix UI';
+export const BASE_URL = 'https://raw.githubusercontent.com/sushant-wayal/Velvix-UI/main/Components/';
 export const SRC = 'src';
 export const COMPONENTS = 'components';
